40ffc972a6dcb49c9b5c79f80c6edad6fa3ba356
Alexander Templeton <Alexander Templeton>
2017-05-08 20:23:51 -0500

    fixed background running process issues

--- bot.py
+++ bot.py
@@ -2,11 +2,10 @@ import logging, telegram
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
 
-bot = telegram.Bot(token='282471240:AAGPCyfKcOsj9C9bqsSVsRXfb_0yuQ-LAJw')
-
-updater = Updater(token='282471240:AAGPCyfKcOsj9C9bqsSVsRXfb_0yuQ-LAJw')
+# TODO: Add proper logging of requests and errors in the future
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+# Define our command handling functions
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="This...is suprisingly easy as fuck.")
 
@@ -17,12 +16,26 @@ def james(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Clayton is still retarded.")
 
 if __name__ == '__main__':
-    #print(bot.get_me())
+
+    bot = telegram.Bot(token='282471240:AAGPCyfKcOsj9C9bqsSVsRXfb_0yuQ-LAJw')
+    updater = Updater(token='282471240:AAGPCyfKcOsj9C9bqsSVsRXfb_0yuQ-LAJw')
+
+    # Override other instances of server running elsewhere
+    bot.setWebhook()
+
+    # Add command handlers
     dispatcher = updater.dispatcher
     start_handler = CommandHandler('start', start)
     #start_handler = CommandHandler('clayton', clayton)
     #start_handler = CommandHandler('james', james)
-    print("Server is running")
 
     dispatcher.add_handler(start_handler)
+
+    # Server has started, log and notify admin
+    print("Server is running")
+
+    # Show our bot server statistics 
+    print(bot.get_me())
+
     updater.start_polling()
+
