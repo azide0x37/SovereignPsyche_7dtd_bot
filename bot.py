b3cf837df20491fcc41a73a25543a6d1c6a2faf0
Alexander Templeton <blakmatr@gmail.com>
2017-05-08 19:21:56 -0500

    added some features :)

--- bot.py
+++ bot.py
@@ -10,9 +10,18 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="This...is suprisingly easy as fuck.")
 
+def clayton(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Clayton is retarded.")
+
+def james(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Clayton is still retarded.")
+
 if __name__ == '__main__':
     #print(bot.get_me())
     dispatcher = updater.dispatcher
     start_handler = CommandHandler('start', start)
+    #start_handler = CommandHandler('clayton', clayton)
+    #start_handler = CommandHandler('james', james)
+
     dispatcher.add_handler(start_handler)
     updater.start_polling()
